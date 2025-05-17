3895977456271dc167092b4fc5f448635f65252b
koval_i <koval.illia.mailbox@gmail.com>
2025-05-17 14:54:12 +0300

    11 lesson

---
+++ funcLes12.py
@@ -0,0 +1,48 @@
+#example 1
+def sum(num_1, num_2):
+    result = num_1 + num_2
+    print(result)
+
+
+sum(10, 20)
+sum(30, 40)
+
+#example 2
+def hi(name):
+    print(f"Hello {name}")
+
+hi("Alex")
+
+# example 3
+def hi(name="Kaleb"):
+    print(f"Hello {name}")
+
+hi()
+
+#example 4
+name="Boris"
+def hi(name):
+    print(f"Hello {name}")
+
+hi(name)
+
+#example 5
+name="Ben"#input()
+#age=32
+def hi(name,age):
+    print(f"Hello! My name is {name}, I`m {age} old")
+
+hi(name,32)
+
+
+#example 6
+name="Tom"
+age="33"
+def hi(name,age):
+    result=name+" "+age
+    return result
+
+h=hi(name,age)
+print(h)
+# print(hi(name,age))
+# print(hi(name,age))
