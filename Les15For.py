b93024e4914dc42e37798b7d18245d712ffa066a
koval_i <koval.illia.mailbox@gmail.com>
2025-05-19 23:11:40 +0300

    Added 14,17 lessons

---
+++ Les15For.py
@@ -0,0 +1,24 @@
+# example 1
+personal = ["Alferd", "Tom", "Ben", "Lessi", "Mary", "Duglas"]
+for i in personal:
+    var="Engineer "+i
+    # print(i,end='')
+    # print(i)
+    # print(var)
+    if i=="Tom":
+        var_1=i+" -- it`s a vonderful engineer"
+        # print(var_1)
+print("######################################################")
+
+# example 2
+# for j in personal[:3]:
+#     print(j)
+for k in personal:
+    print(f"Name {k} have {len(k)} symbols")
+
+
+print("######################################################")
+
+# example 3
+
+print("######################################################")
---
+++ Les17Set.py
@@ -0,0 +1,24 @@
+# example 1. List
+personal_1 = ["Alfred", "Tom", "Ben", "Lessi", "Mary", "Duglas"]
+print(personal_1)
+
+
+print()
+print()
+
+# example 2. Distinct values set()
+personal_2 = {"lessi","Alfred", "Tom", "Ben", "Lessi", "Mary", "Duglas","Tom"}
+print(personal_2)
+
+
+print()
+print()
+
+
+# example 3. Dict
+personal_3 = {"manager":"Alfred", "seller_1":"Tom", "seller_2":"Ben"}
+# print(personal_3)
+# print(personal_3["manager"])
+for k,v in personal_3.items():# k-keys; v-values
+    # print(k)
+    print(v)
